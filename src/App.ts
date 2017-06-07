b7a1e864fdeaa4673adbe4dd32bc91776d05c2ce
pnegi <prinegi100@gmail.com>
2017-06-07 17:40:07 +0530

    Added files

---
+++ src/App.ts
@@ -0,0 +1,50 @@
+import * as path from "path";
+import * as express from "express";
+import * as logger from "morgan";
+import * as bodyParser from "body-parser";
+import HeroRouter from './routes/HeroRouter';
+
+
+class App {
+
+    //ref of express instance 
+    public express : express.Application;
+
+    constructor(){
+        this.express = express();
+        this.middleware();
+        this.routes();
+    }
+
+
+  // Configure Express middleware.
+  private middleware(): void {
+    this.express.use(logger('dev'));
+    this.express.use(bodyParser.json());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+//Configure API endpoints.
+
+
+private routes(): void {
+  /* This is just to get up and running, and to make sure what we've got is
+   * working so far. This function will change when we start to add more
+   * API endpoints */
+  let router = express.Router();
+  // placeholder route handler
+  router.get('/', (req, res, next) => {
+    res.json({
+      message: 'Hello World!'
+    });
+  });
+  this.express.use('/', router);
+  this.express.use('/api/v1/heroes', HeroRouter);
+ }
+}
+
+
+
+
+export default new App().express;
+
